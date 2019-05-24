11f2516b906f328858d4cf79c7a0e333705da9be
apple <M18342207861@163.COM>
2019-05-24 11:31:30 +0800

    第二次作业： 素数因子 约瑟夫问题

---
+++ homework2.cs
@@ -0,0 +1,54 @@
+using System;
+class Program
+{
+    static void Main()
+{
+    Console.WriteLine("请输入一个数据");
+    a=Console.ReadLine();
+    for(i=2;i<a;i++)
+    {
+        if(a%i==0)
+        {
+            return i;
+            Console.WriteLine("所有素数因子是"+i);
+        }
+    }
+    return a;
+}
+}
+class YSF
+{
+    static void main()
+{
+    int josef(int a[],12,5)
+    {
+        int i,j,k=0;
+        for(i=0;i<12;i++)
+        {
+            j=1;
+            while(j<5)
+            {
+                while(a[k]==0)
+                k=(k+1)%n;
+                j++;
+                k=(k+1)%n;
+            }
+            while(a[k]=0)
+            k=(k+1)%n;
+            Console.WriteLine("%d",a[k]);
+            a[k]=0;
+        }
+        return 0;
+    }
+    int main()
+    {
+        int a[100];
+        int i,j;
+        for(i=0;i<12;i++)
+            a[i]=i+1;
+        Console.WriteLine("\n output:\n");
+        josef(a,12,5);
+        return 0；
+    }
+}
+}
